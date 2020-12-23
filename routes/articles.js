e85722a9c039b63010864d16d5e79fd0d6d952f3
Tanvir <tave_94@hotmail.com>
2020-12-23 18:22:06 +0100

    added mongodb and new router

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req, res) => {
-    res.render('articles/new')
+    res.render('articles/new', {article: new Article()})
 })
 
 router.post('/', (req, res) => {
+const article = new Article({
 
+})
 })
 
 module.exports = router
