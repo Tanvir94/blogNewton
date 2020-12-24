3e5f993f4e82facd3541c8f01ca40a3525ed6e42
Tanvir <tave_94@hotmail.com>
2020-12-24 19:25:41 +0100

    added show and article

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
 
 app.use('/articles', articleRouter)
-
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test descprition'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test descprition 2'
-    }]
-res.render('articles/index', {articles: articles })
-})
 
 app.listen(5000)
